f8a6e14617ebb77577a34e724926ed25239af4ac
jupyter <koemheang200@gmail.com>
2026-05-17 15:13:59 +0700

    Initial Commit

---
+++ StartUpSwiftUiEp0/StartUpSwiftUiEp0App.swift
@@ -0,0 +1,17 @@
+//
+//  StartUpSwiftUiEp0App.swift
+//  StartUpSwiftUiEp0
+//
+//  Created by Jupyter on 17/5/26.
+//
+
+import SwiftUI
+
+@main
+struct StartUpSwiftUiEp0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StartUpSwiftUiEp0Tests/StartUpSwiftUiEp0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  StartUpSwiftUiEp0Tests.swift
+//  StartUpSwiftUiEp0Tests
+//
+//  Created by Jupyter on 17/5/26.
+//
+
+import Testing
+@testable import StartUpSwiftUiEp0
+
+struct StartUpSwiftUiEp0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
